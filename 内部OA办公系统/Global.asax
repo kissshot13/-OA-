bd6053dc2e4c3fb554ed4c6dfd5b6b3865b3ce68
liwei <liwei@YYS-LGS>
2017-06-01 15:29:34 +0800

    添加项目文件。

---
+++ 内部OA办公系统/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="内部OA办公系统.MvcApplication" Language="C#" %>
---
+++ 内部OA办公系统/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(内部OA办公系统.Startup))]
+namespace 内部OA办公系统
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
